57d3f9b430d9cfb12877e5c75a5d92f9f792fb42
Julien de Castelnau <julien.decastelnau@epfl.ch>
2024-05-22 17:32:09 +0200

    bsp.h in its own header file

--- sw/single/src/sync_test.c
+++ sw/single/src/sync_test.c
@@ -1,47 +1,7 @@
 #include "../mmio.h"
-
-
-int bspQueue[1024];
-char bspShadowQueue[1024];
-int* bspQueuePtr = bspQueue;
-char* bspShadowQueuePtr = bspShadowQueue;
+#include "../bsp.h"
 
 const int c0_data[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
-
-void bsp_put(int core, int* source, void* dest, int len) {
-    if (len < 1 || len > 16) return; // not allowed to do more than 16 burst
-    // head flit
-    *(bspShadowQueuePtr++) = 0;
-    *(bspQueuePtr++) = (((int)dest)&0x3FFF) | ((len-1) << 14) | ((core&0xF) << 18);
-    // body
-    for (int i = 0; i < len; i++) {
-        *(bspShadowQueuePtr++) = 1;
-        *(bspQueuePtr++) = (int)(*(source++));
-    }
-    *(bspShadowQueuePtr++) = 2;
-    *(bspQueuePtr++) = 0;
-}
-
-void bsp_sync() {
-    *BSP_MY_SYNC = 1;
-    while (!*BSP_ALL_SYNC_START);
-    *BSP_ALL_SYNC_START = 0;
-
-    int* bspRdPtr = bspQueue;
-    char* bspShadowRdPtr = bspShadowQueue;
-    while (bspRdPtr != bspQueuePtr) {
-        char type = *(bspShadowRdPtr++);
-        int data = *(bspRdPtr++);
-        *(ROUTER_SEND_FLIT_H+type) = data;
-    }
-    bspQueuePtr = bspQueue;
-    bspShadowQueuePtr = bspShadowQueue;
-
-    // copy the buffer
-    *BSP_MY_SYNC = 0;
-    while (!*BSP_ALL_SYNC_END);
-    *BSP_ALL_SYNC_END = 0;
-}
 
 void puts(char* string) {
     while (*string != 0) {
